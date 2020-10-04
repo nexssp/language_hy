7a13266b44c73f5699f5e0ad3b45b8bcb30e549f
Marcin Polak <mapoart@gmail.com>
2020-10-05 00:02:00 +0200

    Ubuntu

--- hy.linux.nexss.config.js
+++ hy.linux.nexss.config.js
@@ -1,14 +1,15 @@
 let languageConfig = Object.assign({}, require("./hy.win32.nexss.config"));
-const os = require("@nexssp/os");
+const os = require(`${process.env.NEXSS_SRC_PATH}/node_modules/@nexssp/os/`);
+const distName = os.name();
+languageConfig.dist = distName;
 
 const sudo = os.sudo();
-const distName = os.name();
 
-languageConfig.dist = distName;
 languageConfig.compilers = {
   hy: {
-    install: `${sudo}apt-get install -y python
-${sudo}python -m pip install hy`,
+    install: `${sudo}apt-get install -y python3
+${sudo}apt-get install python3-pip
+${sudo}python3 -m pip install hy`,
     command: "hy",
     args: "<file>",
     help: ``,
@@ -20,6 +21,12 @@ switch (distName) {
   //   languageConfig.compilers.hy.install =
   //     "pacman -S --noconfirm python & python -m pip install hy"; // error: package org.json does not exist
   //   break;
+  case os.distros.UBUNTU:
+    languageConfig.compilers.hy.install = os.replacePMByDistro(
+      `${sudo}apt install -y python3-dev && ` +
+        languageConfig.compilers.hy.install
+    );
+    break;
   default:
     languageConfig.compilers.hy.install = os.replacePMByDistro(
       languageConfig.compilers.hy.install
